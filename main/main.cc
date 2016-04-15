681229070cd64e61f2a1b6418b34d399f65c336d
Alexander Scheel <alexander.m.scheel@gmail.com>
2016-04-15 16:16:00 -0500

    Finished ECB block mode

--- main/main.cc
+++ main/main.cc
@@ -5,9 +5,11 @@
 */
 
 #include "../src/blowfish.hh"
+#include "../src/blowfish_ecb.hh"
 
 #include <cstdint>
 #include <iostream>
+#include <cstring>
 #include "stdio.h"
 
 using namespace std;
@@ -18,14 +20,7 @@ int main(int argc, char* argv[])
     int length = 7;
     blowfish* ciph = new blowfish(key_data, length);
     char* plain_text= (char*) malloc(sizeof(char) * 8);
-    plain_text[0] = 'H';
-    plain_text[1] = 'I';
-    plain_text[2] = ' ';
-    plain_text[3] = 'A';
-    plain_text[4] = 'L';
-    plain_text[5] = 'L';
-    plain_text[6] = '!';
-    plain_text[7] = '\0';
+    strcpy(plain_text, "Hi All!");
 
     uint32_t zero_left = 1;
     uint32_t zero_right = 2;
@@ -38,9 +33,25 @@ int main(int argc, char* argv[])
     char* cipher_text = ciph->encrypt(plain_text);
     char* plaintext = ciph->decrypt(cipher_text);
 
-    cout << plain_text << " " << cipher_text << " " << plaintext << endl;
+    cout << plain_text << " "  << plaintext << endl;
 
     free(cipher_text);
     free(plaintext);
+
+    char* plain_text_2 = (char*) malloc(sizeof(char) * 40);
+    strncpy(plain_text_2, "12345678 Hello World! This is a test.78", 40);
+
+    blowfish_ecb* ciph2 = new blowfish_ecb(key_data, length);
+    char* cipher_text_2 = ciph2->block_encrypt(plain_text_2, 40);
+    char* plaintext2 = ciph2->block_decrypt(cipher_text_2, 40);
+
+    cout << plain_text_2 << " " << plaintext2 << endl;
+
+    free(plain_text);
+    free(plain_text_2);
+    free(cipher_text_2);
+    free(plaintext2);
+
     delete ciph;
+    delete ciph2;
 }
--- src/blowfish_ecb.cc
+++ src/blowfish_ecb.cc
@@ -16,50 +16,66 @@ blowfish_ecb::blowfish_ecb(char* key_data, int key_length) : blowfish(key_data, 
 
 char* blowfish_ecb::block_encrypt(char* data, int data_length)
 {
+    if (data_length % 8 != 0) {
+        // TODO: throw error
+        return NULL;
+    }
+
     uint32_t left;
     uint32_t right;
     char* result;
+    int i = 0;
 
-    result = (char *) malloc(sizeof(char) * 8);
+    result = (char *) malloc(sizeof(char) * data_length);
 
-    left = ((uint8_t) data[0] << 24) + ((uint8_t) data[1] << 16) + ((uint8_t) data[2] << 8) + (uint8_t) data[3];
-    right = ((uint8_t) data[4] << 24) + ((uint8_t) data[5] << 16) + ((uint8_t) data[6] << 8) + (uint8_t) data[7];
+    for (i = 0; i < data_length; i+=8) {
+        left = ((uint8_t) data[i+0] << 24) + ((uint8_t) data[i+1] << 16) + ((uint8_t) data[i+2] << 8) + (uint8_t) data[i+3];
+        right = ((uint8_t) data[i+4] << 24) + ((uint8_t) data[i+5] << 16) + ((uint8_t) data[i+6] << 8) + (uint8_t) data[i+7];
 
-    this->encrypt(&left, &right);
+        this->encrypt(&left, &right);
 
-    result[0] = (left & 0xFF000000) >> 24;
-    result[1] = (left & 0x00FF0000) >> 16;
-    result[2] = (left & 0x0000FF00) >> 8;
-    result[3] = left & 0x000000FF;
-    result[4] = (right & 0xFF000000) >> 24;
-    result[5] = (right & 0x00FF0000) >> 16;
-    result[6] = (right & 0x0000FF00) >> 8;
-    result[7] = right & 0x000000FF;
+        result[i+0] = (left & 0xFF000000) >> 24;
+        result[i+1] = (left & 0x00FF0000) >> 16;
+        result[i+2] = (left & 0x0000FF00) >> 8;
+        result[i+3] = left & 0x000000FF;
+        result[i+4] = (right & 0xFF000000) >> 24;
+        result[i+5] = (right & 0x00FF0000) >> 16;
+        result[i+6] = (right & 0x0000FF00) >> 8;
+        result[i+7] = right & 0x000000FF;
+    }
     return result;
 
 }
 
 char* blowfish_ecb::block_decrypt(char* data, int data_length)
 {
+    if (data_length % 8 != 0) {
+        // TODO: throw error
+        return NULL;
+    }
+
     uint32_t left;
     uint32_t right;
     char* result;
+    int i = 0;
 
-    result = (char *) malloc(sizeof(char) * 8);
+    result = (char *) malloc(sizeof(char) * data_length);
 
-    left = ((uint8_t) data[0] << 24) + ((uint8_t) data[1] << 16) + ((uint8_t) data[2] << 8) + (uint8_t) data[3];
-    right = ((uint8_t) data[4] << 24) + ((uint8_t) data[5] << 16) + ((uint8_t) data[6] << 8) + (uint8_t) data[7];
+    for (i = 0; i < data_length; i+=8) {
+        left = ((uint8_t) data[i+0] << 24) + ((uint8_t) data[i+1] << 16) + ((uint8_t) data[i+2] << 8) + (uint8_t) data[i+3];
+        right = ((uint8_t) data[i+4] << 24) + ((uint8_t) data[i+5] << 16) + ((uint8_t) data[i+6] << 8) + (uint8_t) data[i+7];
 
-    this->decrypt(&left, &right);
+        this->decrypt(&left, &right);
 
-    result[0] = (left & 0xFF000000) >> 24;
-    result[1] = (left & 0x00FF0000) >> 16;
-    result[2] = (left & 0x0000FF00) >> 8;
-    result[3] = left & 0x000000FF;
-    result[4] = (right & 0xFF000000) >> 24;
-    result[5] = (right & 0x00FF0000) >> 16;
-    result[6] = (right & 0x0000FF00) >> 8;
-    result[7] = right & 0x000000FF;
+        result[i+0] = (left & 0xFF000000) >> 24;
+        result[i+1] = (left & 0x00FF0000) >> 16;
+        result[i+2] = (left & 0x0000FF00) >> 8;
+        result[i+3] = left & 0x000000FF;
+        result[i+4] = (right & 0xFF000000) >> 24;
+        result[i+5] = (right & 0x00FF0000) >> 16;
+        result[i+6] = (right & 0x0000FF00) >> 8;
+        result[i+7] = right & 0x000000FF;
+    }
 
     return result;
 }
